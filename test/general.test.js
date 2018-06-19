3545b3010d589c215cb104bf8ca3d27f600c49bf
Anthony Hildoer <anthony@bluerival.com>
2018-06-19 15:46:25 -0600

    improve boiler plate test

--- test/general.test.js
+++ test/general.test.js
@@ -5,6 +5,10 @@ const async = require( 'async' );
 const SetupTestConfigComponent = require( './lib/test.config' );
 const StringStackCore = require( '@stringstack/core' );
 
+// lets you pull a component from the stack, do not use this pattern of accessing _loader outside of testing
+let getComponentNative = function ( app, targetPath ) {
+  return app._loader.get( 'app', targetPath );
+};
 
 describe( 'stringstack', function () {
   describe( 'base', function () {
@@ -12,6 +16,7 @@ describe( 'stringstack', function () {
     it( 'should instantiate, init and dinit', function ( done ) {
 
       let app = null;
+      let component = null;
 
       async.series( [
         ( done ) => {
@@ -34,11 +39,29 @@ describe( 'stringstack', function () {
 
             app = new App( 'test' ); // Instantiates all dependencies
 
-            done();
+            component = getComponentNative( app, './index' );
+
+            assert( component, 'component should be available' );
 
           } catch ( e ) {
             return done( e );
           }
+
+          done();
+
+        },
+        ( done ) => {
+
+          try {
+
+            // do not use the pattern of accessing _config externally outside of testing
+            assert.strictEqual( component._config, null, 'component config should be null' );
+
+          } catch ( e ) {
+            return done( e );
+          }
+
+          done();
 
         },
         ( done ) => {
@@ -49,11 +72,39 @@ describe( 'stringstack', function () {
           }
         },
         ( done ) => {
+
+          try {
+
+            assert.equal( JSON.stringify( component._config, null, 4 ),
+              JSON.stringify( SetupTestConfigComponent.defaultConfig, null, 4 ),
+              'component config should be set' );
+
+          } catch ( e ) {
+            return done( e );
+          }
+
+          done();
+
+        },
+        ( done ) => {
           try {
             app.dinit( done ); // D-initializes all dependencies
           } catch ( e ) {
             return done( e );
           }
+        },
+        ( done ) => {
+
+          try {
+
+            assert.strictEqual( component._config, null, 'component config should be null' );
+
+          } catch ( e ) {
+            return done( e );
+          }
+
+          done();
+
         }
       ], done );
 
